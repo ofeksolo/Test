a006b53b45083a75902dfe84b2a37b899418c177
ofek <ofek>
2020-08-15 15:52:22 +0300

    added if statement

--- print_job_name.py
+++ print_job_name.py
@@ -5,9 +5,12 @@ import sys
 jenkins_server = jenkins.Jenkins('http://localhost:8080/', username='admin', 
                          password='Aa123456')
 job = sys.argv[1]
-
-Job_name = jenkins_server.get_job_name(job)
-print ("The name of the job that initiated me is {}".format(Job_name))
+job_name = jenkins_server.get_job_name(job)
+if job_name == None:
+    print ("Error: There is no such job {}".format(job))
+    sys.exit(1)
+else:
+    print ("The name of the job that initiated me is {}".format(job_name))
 
 # Could also do it by only printing the sys.argv
 # As im passing env.JOB_NAME as argument when running the script from Jenkins.
